d54a920d723a8abf6b1c9a0ce5ec40e0b8041275
BarcelossRuan <ruan.elesis@escola.pr.gov.br>
2026-06-25 13:53:20 -0300

    Add mostrarNome function to display user name

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,9 @@
     function revelarMensagem () {
         document.getElementById("mensagem").innerText = "Olá, mundo! Aprendendo JS";
     }
+
+    function mostrarNome() {
+        let nome = document.getElementById("nomeUsuario").Value;
+
+        document.getElementById("resultadoNome").innerText = "Bem-vindo, " + nome + "!"
+    }
